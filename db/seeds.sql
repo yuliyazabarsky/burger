af8468901b1f11c543f9b19566c403425394f038
yuliyazabarsky <yuliya1975@msn.com>
2019-04-10 23:29:03 -0600

    adding files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ("Turkey");
+INSERT INTO burgers (burger_name) VALUES ("Cheesy");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Quinoa", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bbq", true);
+INSERT INTO burgers (burger_name) VALUES ("Slider");
+INSERT INTO burgers (burger_name) VALUES ("Salmon");
