f699c0d468bdb45d030f1d3306c105c84fb7e8c7
yuliyazabarsky <yuliya1975@msn.com>
2019-04-12 17:16:58 -0600

    adding changes

--- config/orm.js
+++ config/orm.js
@@ -16,30 +16,30 @@ function printQuestionMarks(num) {
   return arr.toString();
 }
 
-// Helper function to convert object key/value pairs to SQL syntax
+// // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
 
-  // loop through the keys and push the key/value as a string int arr
+//   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+//       // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
+//       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+//       // e.g. {devoured: true} => ["devoured=true"]
       arr.push(key + "=" + value);
     }
   }
 
-  // translate array of strings to a single comma-separated string
+//   // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
-// Object for all our SQL statement functions.
+// // Object for all our SQL statement functions.
 var orm = {
   all: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
@@ -70,7 +70,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+//   // An example of objColVals would be {name: chickpeas, devoured: true}
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -103,5 +103,5 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (burger.js).
+// // Export the orm object for the model (burger.js).
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,53 @@ var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+  router.post("/api/burgers", function(req, res) {
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+
+  router.put("/api/burgers/:id", function(req, res){
+      var condition = "id = " + req.params.id;
+
+      console.log("condition", condition);
+
+      burger.update({
+        devoured: req.body.devoured
+      }, condition, function(result){
+        if (result.changedRows == 0){
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      });
+  });
+
+  router.delete("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+    burger.delete(condition, function(result){
+      if (result.affectedRows==0){
+        return res.status(404).end();
+      }else{
+        res.status(200).end();
+      }
+  });
+})
+
+module.exports = router;
