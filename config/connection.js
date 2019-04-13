bdbf110b2b9dca5ece97b9c75f7d391282767ed3
yuliyazabarsky <yuliya1975@msn.com>
2019-04-12 23:06:26 -0600

    changes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
+
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSBD_URL);
@@ -15,13 +15,7 @@ if(process.env.JAWSDB_URL) {
 };
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
