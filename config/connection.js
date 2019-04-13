8605adb7d886a85617b04120bbb88c3761540d73
yuliyazabarsky <yuliya1975@msn.com>
2019-04-13 12:16:46 -0600

    heroku again

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,43 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
-var connection;
+// var mysql = require("mysql");
+// var connection;
 
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSBD_URL);
-} else {
-  connection = mysql.createConnection ({
+// if(process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSBD_URL);
+// } else {
+//   connection = mysql.createConnection ({
+//   host: "localhost",
+//   port: 8889,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
+// };
+
+// Make connection.
+// connection.connect();
+
+// Export connection for our ORM to use.
+// module.exports = connection;
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
   user: "root",
   password: "root",
   database: "burgers_db"
 });
-};
 
 // Make connection.
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
