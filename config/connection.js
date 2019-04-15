a0c33cbb833d7882672a125604261cf395b4b4fa
yuliyazabarsky <yuliya1975@msn.com>
2019-04-15 09:48:16 -0600

    try again

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-// var connection;
+var connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSBD_URL);
@@ -20,7 +20,7 @@ connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
 
-var mysql = require("mysql");
+
 
 // var connection = mysql.createConnection({
 //   host: "localhost",
